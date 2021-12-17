35c9a3b8f74cab23c68d601db957450f5bfe53fe
chaenabi <find1086@gmail.com>
2021-12-17 17:02:05 +0900

    Initial commit

---
+++ src/main/java/io/springbatch/springbatchlecture/SpringbatchlectureApplication.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchlectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchlectureApplication.class, args);
+    }
+
+}
